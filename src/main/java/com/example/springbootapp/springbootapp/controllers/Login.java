ceadc801f4c7ab57e94e04fb300bdc5eb716314d
pravinv1998 <pravin.wargad1998@gmail.com>
2026-01-16 22:37:09 +0530

    login api

--- src/main/java/com/example/springbootapp/springbootapp/controllers/Login.java
+++ src/main/java/com/example/springbootapp/springbootapp/controllers/Login.java
@@ -10,4 +10,14 @@ public class Login {
         return "Login Page";
     }
 
+    @GetMapping("/logOut")
+    public String logOut() {
+        return "Logout Page.";
+    }
+
+    @GetMapping("/forgotPassword")
+    public String forgotPassword() {
+        return "Forgot Password";
+    }
+
 }
